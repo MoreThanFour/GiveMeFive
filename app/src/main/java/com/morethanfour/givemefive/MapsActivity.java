7bc9881eb76ec9cfd2a9ebbe5178357d4db6a39b
Valentin Bercot <valent1.bercot@gmail.com>
2015-11-13 15:59:55 +0800

    Update method to display maps to current location

--- app/src/main/java/com/morethanfour/givemefive/MapsActivity.java
+++ app/src/main/java/com/morethanfour/givemefive/MapsActivity.java
@@ -17,13 +17,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -37,12 +37,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // Configure Google Maps
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Get current lat long of connected user
+        LatLng myPosition = new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
+
+        // Add a marker in current location and move the camera to this location
+        mMap.addMarker(new MarkerOptions().position(myPosition));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
     }
-
-
 }
