d3d07aeed5f43c20591ecaf1e021d1ff9f759edc
Dorian Blanc <blanc.dorian.pro@gmail.com>
2015-11-13 15:12:39 +0800

    DispatchActivity: Get FriendList and send it to Main Activity

--- app/src/main/java/com/morethanfour/givemefive/DispatchActivity.java
+++ app/src/main/java/com/morethanfour/givemefive/DispatchActivity.java
@@ -5,7 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DispatchActivity extends Activity {
 
@@ -18,11 +25,28 @@ public class DispatchActivity extends Activity {
 
         // Start an intent for the login activity if the user is not logged in
         // or the main activity if the user il already logged
-        if (ParseUser.getCurrentUser() != null){
 
-            startActivity(new Intent(this, MainActivity.class));
-            //AUTO LOG OUT
-            ParseUser.logOut();
+        if (ParseUser.getCurrentUser() != null){
+            new GraphRequest(
+                    AccessToken.getCurrentAccessToken(),
+                    "/me/friends",
+                    null,
+                    HttpMethod.GET,
+                    new GraphRequest.Callback() {
+                        public void onCompleted(GraphResponse response) {
+                            //ParseUser.logOut();
+                            JSONObject data = response.getJSONObject();
+
+                            try {
+                                Intent toMainActivity = new Intent(DispatchActivity.this, MainActivity.class);
+                                toMainActivity.putExtra("FriendList", data.getJSONArray("data").toString());
+                                startActivity(toMainActivity);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            ).executeAsync();
         }
         else{
             startActivity(new Intent(this, LoginActivity.class));
