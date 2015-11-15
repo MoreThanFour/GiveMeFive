5127e7a6d9b1ac01d564079868a15a9e953e8b78
Valentin Bercot <valent1.bercot@gmail.com>
2015-11-16 01:33:03 +0800

    Set cloud code method to notify user

--- Cloud_Code/cloud/main.js
+++ Cloud_Code/cloud/main.js
@@ -1,6 +1,27 @@
+// Method used to send Push Notifications to each user who have friends close to him.
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+Parse.Cloud.define("hello", function(request, response)
+{
+    // And we push notification to the current user's Installation.
+    var queryInstallation = new Parse.Query(Parse.Installation);
+    queryInstallation.equalTo('deviceToken', request.object.get('deviceToken'));
+
+    Parse.Push.send({
+        where: queryInstallation,
+        data: {
+            alert: "Hey! " + request.object.get('name') + " is really close to you. Go give him five!";
+        },
+        success: function()
+        {
+            // Push was successful
+            alert("It works");
+        },
+        error: function(error)
+        {
+            // Handle error
+            alert("Hummm, I miss something");
+        }
+    })
+
+    response.success();
+}
